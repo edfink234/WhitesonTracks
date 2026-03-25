f21e5d9bf867a088eabeb56de5b6c1d7b127b5c5
edfink234 <84761455+edfink234@users.noreply.github.com>
2026-03-24 19:12:41 -0700

    track idx 3

--- src/pareto_front_track_progression_track_idx_3.py
+++ src/pareto_front_track_progression_track_idx_3.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Data extracted from your log
-max_sizes = [9, 17, 24, 34, 43, 53, 62, 64, 70, 71, 79, 81, 91, 101, 103, 151, 202, 211, 209]
-sse_values = [8929.28, 1434, 125.605, 63.3617, 47.1009, 13.0276, 9.09682, 6.02855, 5.13096, 4.91967, 3.75413, 3.28463, 3.21014, 3.1258, 2.75353, 1.04649, 0.33099, 0.325704, 0.212326]
+max_sizes = [9, 17, 24, 34, 43, 53, 62, 64, 70, 71, 79, 81, 91, 101, 103, 151, 202, 211, 208, 227, 249, 268]
+sse_values = [8929.28, 1434, 125.605, 63.3617, 47.1009, 13.0276, 9.09682, 6.02855, 5.13096, 4.91967, 3.75413, 3.28463, 3.21014, 3.1258, 2.75353, 1.04649, 0.33099, 0.325704, 0.212326, 0.135493, 0.104753, 0.101144]
 
 assert(len(max_sizes) == len(sse_values))
               
@@ -16,7 +16,7 @@ plt.plot(max_sizes, sse_values, marker='o', linestyle='-', color='#2c3e50', line
 
 # Annotations for Old Progression
 for i, d in enumerate(depths):
-    if d in [4, 10, 17]:
+    if d in [4, 10, 17, 23]:
         plt.annotate(f'Depth {d}', (max_sizes[i], sse_values[i]), textcoords="offset points", xytext=(0,10), ha='center', color='#2c3e50')
 
 # Log scale for Y-axis often makes SR Pareto plots easier to read
